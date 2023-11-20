a617d30a6beb251919229c7aabbfeac1723462db
Anthony Moya Ochoa <anthonymoya246@gmail.com>
2023-11-20 09:05:36 -0500

    Subir todos los cambios realizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { AddQualificationComponent } from './pages/add-qualification/add-qualification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'add-qualification', component: AddQualificationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
+import { AddQualificationComponent } from './pages/add-qualification/add-qualification.component';
+import { MenuComponent } from './layouts/menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    IndexComponent,
+    AddQualificationComponent,
+    MenuComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
